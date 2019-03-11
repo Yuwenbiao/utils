5ccc87195b0608c1ac6b2a14357158ce01881d4c
Yuwenbiao <ywb1996@189.cn>
2019-03-11 17:12:05 +0800

    lombok示例

---
+++ src/main/java/com/example/util/lombok/lombok示例/LombokDemo.java
@@ -0,0 +1,16 @@
+package com.example.util.lombok.lombok示例;
+
+import lombok.Data;
+
+/**
+ * lombok示例
+ *
+ * @author yuwb@corp.21cn.com
+ * @date 19-3-11 下午5:03
+ */
+@Data
+public class LombokDemo {
+    private String id;
+    private String name;
+    private String book;
+}
