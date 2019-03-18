2fec9c43d2bc6425b58a4825c9d70dc94598e17f
Yuwenbiao <ywb1996@189.cn>
2019-03-18 17:17:56 +0800

    将数据集合写入到Excel中

--- src/main/java/com/example/util/文件操作/poi/ParseExcel.java
+++ src/main/java/com/example/util/文件操作/poi/ParseExcel.java
@@ -1,9 +1,13 @@
 package com.example.util.文件操作.poi;
 
+import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 
 /**
  * 解析Excel文件
@@ -26,6 +30,32 @@ public class ParseExcel {
             for (int i = 0; i <= lastRowNum; i++) {
                 System.out.println(sheet.getRow(i).getCell(1).getStringCellValue());
             }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 将数据集合写入到Excel中
+     *
+     * @param dataList 数据集合
+     */
+    public static void writeExcel(List<Map> dataList) {
+        XSSFWorkbook xssfWorkbook;
+        try (FileOutputStream outputStream = new FileOutputStream("/home/ywb/Documents/test/testO.xlsx")) {
+            xssfWorkbook = new XSSFWorkbook();
+            XSSFSheet sheet = xssfWorkbook.createSheet();
+            int lastRowNum = dataList.size();
+            for (int i = 0; i < lastRowNum; i++) {
+                Row row = sheet.createRow(i);
+                Map dataMap = dataList.get(i);
+
+                int cellIndex = 0;
+                for (Object value : dataMap.values()) {
+                    row.createCell(cellIndex++).setCellValue(String.valueOf(value));
+                }
+            }
+            xssfWorkbook.write(outputStream);
         } catch (IOException e) {
             e.printStackTrace();
         }
