ffefc57fb7a33224fa22e3c7421aa13bff490c64
余文彪 <yuwb@corp.21cn.com>
2019-11-04 16:42:17 +0800

    - 集合排序

--- src/main/java/com/example/util/集合操作/ListDemo.java
+++ src/main/java/com/example/util/集合操作/ListDemo.java
@@ -1,6 +1,7 @@
 package com.example.util.集合操作;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -13,8 +14,10 @@ import java.util.stream.Collectors;
 public class ListDemo {
     public static void main(String[] args) {
 //        subList();
-        filterList();
+//        filterList();
+        sortDemo();
     }
+
 
     /**
      * 初始化List
@@ -37,5 +40,24 @@ public class ListDemo {
     private static void filterList() {
         List<String> list = getList();
         System.out.println(list.stream().filter(s -> !"a".equals(s)).collect(Collectors.toList()));
+    }
+
+    /**
+     * 排序
+     */
+    private static void sortDemo() {
+        List<TestDemo> testDemos = Arrays.asList(new TestDemo("b"), new TestDemo("a"));
+        testDemos.sort(Comparator.comparing(a -> a.time));
+        for (TestDemo testDemo : testDemos) {
+            System.out.println(testDemo.time);
+        }
+    }
+
+    static class TestDemo {
+        String time;
+
+        public TestDemo(String time) {
+            this.time = time;
+        }
     }
 }
